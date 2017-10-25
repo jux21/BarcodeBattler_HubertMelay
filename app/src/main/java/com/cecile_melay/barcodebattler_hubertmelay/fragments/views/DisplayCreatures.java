1c14955a59ed3326240cffdb08bc54fd33134ef5
Cecile <cemelay@gmail.com>
2017-10-25 10:28:11 +0200

    Add views and their links on the main menu + update barcodescanner

---
+++ app/src/main/java/com/cecile_melay/barcodebattler_hubertmelay/fragments/views/DisplayCreatures.java
@@ -0,0 +1,21 @@
+package com.cecile_melay.barcodebattler_hubertmelay.fragments.views;
+
+import com.cecile_melay.barcodebattler_hubertmelay.R;
+import com.cecile_melay.barcodebattler_hubertmelay.fragments.MyFragment;
+
+/**
+ * Created by Utilisateur on 25/10/2017.
+ */
+
+public class DisplayCreatures extends MyFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.display_creatures;
+    }
+
+    @Override
+    protected String getTitle() {
+        return null;
+    }
+}
