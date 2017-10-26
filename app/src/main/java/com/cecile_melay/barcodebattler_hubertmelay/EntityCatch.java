b163025898f72683e8b56bdc7a29dbeb2ea5c220
Cecile <cemelay@gmail.com>
2017-10-26 09:55:30 +0200

    update entity catch : begin hash

--- app/src/main/java/com/cecile_melay/barcodebattler_hubertmelay/EntityCatch.java
+++ app/src/main/java/com/cecile_melay/barcodebattler_hubertmelay/EntityCatch.java
@@ -6,7 +6,10 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Switch;
 
+import com.cecile_melay.barcodebattler_hubertmelay.database.dao.CreatureDAO;
+import com.cecile_melay.barcodebattler_hubertmelay.entities.Creature;
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -48,26 +51,121 @@ public class EntityCatch extends Activity implements ZXingScannerView.ResultHand
 
         String barCodeFormat = rawResult.getBarcodeFormat().toString();
         String result = rawResult.getText();
-        // afficher le résultat dans une dialog box.
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Félicitation, entité capturée !");
-        builder.setMessage(result+  " - " + barCodeFormat +
-            "\n\n\n Souhaitez vous capturer d'autres entitié")
-            .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        }).setNegativeButton("Non",new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
 
-        AlertDialog alerte = builder.create();
-        alerte.show();
+        String message = createEntity(barCodeFormat, result);
+
+        displayAlertDialog(message, barCodeFormat, result);
 
         ScannerView.resumeCameraPreview(this);
+    }
+
+    private void displayAlertDialog(String message, String barCodeFormat, String result) {
+
+        AlertDialog alterDialog = new AlertDialog.Builder(this)
+            .setTitle(message)
+            .setMessage(result+  " - " + barCodeFormat +
+                    "\n \n Souhaitez vous capturer d'autres entités ?")
+            .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            })
+            .setNegativeButton("Non",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            })
+            .create();
+        alterDialog.show();
+    }
+
+    private String createEntity(String barCodeFormat, String result) {
+        String message;
+        if (result != null) {
+            switch (barCodeFormat.toUpperCase()) {
+
+                //code bar utilisé dans le commerce et l'industrie
+                case "EAN":
+                    createCreature(result);
+                    message = "Félicitation, créature capturée !";
+                    break;
+                case "EAN_8":
+                    createCreature8(result);
+                    message = "Félicitation, créature capturée !";
+                    break;
+                case "EAN_13":
+                    createCreature13(result);
+                    message = "Félicitation, créature capturée !";
+                    break;
+
+                //code bar utilisé en pharmacie
+                case "CODE_39":
+                    createPotion(result);
+                    message = "Félicitation, potion capturée !";
+                    break;
+                case "CODE_93":
+                    createPotion(result);
+                    message = "Félicitation, potion capturée !";
+                    break;
+
+                //code bar QR Code
+                case "CODE_QR":
+                    message = "Félicitation, équipement capturé !";
+                    createEquipment(result);
+                    break;
+
+                //le format du code n'est pas reconnu
+                default:
+                    message = "Oups, vous n'avez rien capturé";
+                    break;
+            }
+        } else {
+            message = "Oups, vous n'avez rien capturé";
+        }
+        return message;
+    }
+
+    private void createEquipment(String result) {
+
+    }
+
+    private void createCreature8(String result) {
+
+    }
+
+    private void createCreature13(String result) {
+        String test = "0123456789012";
+        char[] array = result.toCharArray();
+
+        for (int i = 0; i < array.length; i++) {
+            //array[i];
+        }
+        String name;
+        int hp;
+        String type;
+        int inventory_max_size;
+        int size;
+        int weight;
+        int defense;
+
+       // Creature creature13 = new Creature(name,  hp,  type,  inventory_max_size,  size,  weight,  defense);
+       // persistCreature(creature13);
+    }
+
+    private void createPotion(String result) {
+
+    }
+
+    private void createCreature(String result) {
+
+
+    }
+
+    private void persistCreature(Creature creature) {
+        CreatureDAO creatureDAO = new CreatureDAO(this);
+        creatureDAO.open();
+        creatureDAO.insertCreature(creature);
     }
 }
